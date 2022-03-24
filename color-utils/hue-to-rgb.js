cc993097bf31d88a10d6fd392b305ff2f294e4c7
tiye <jiyinyiyong@gmail.com>
2022-03-25 00:58:04 +0800

    upgrade calcit; add support for hclx; tag 0.4.27

--- color-utils/hue-to-rgb.js
+++ color-utils/hue-to-rgb.js
@@ -1,19 +1,22 @@
-// https://stackoverflow.com/a/36722579/883571
 
-export let hslToRgb = function hslToRgb(h, s, l) {
+import { hcl } from "d3-color";
+
+var hue2rgb = function hue2rgb(p, q, t) {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1 / 6) return p + (q - p) * 6 * t;
+  if (t < 1 / 2) return q;
+  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+  return p;
+};
+
+// https://stackoverflow.com/a/36722579/883571
+export let hsl_to_rgb = (h, s, l) => {
   var r, g, b;
 
   if (s == 0) {
     r = g = b = l; // achromatic
   } else {
-    var hue2rgb = function hue2rgb(p, q, t) {
-      if (t < 0) t += 1;
-      if (t > 1) t -= 1;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
-    };
 
     var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
     var p = 2 * l - q;
@@ -24,3 +27,8 @@ export let hslToRgb = function hslToRgb(h, s, l) {
 
   return [r, g, b];
 }
+
+export let hcl_to_hex = (h, c, l) => {
+  let color = hcl(h, c, l);
+  return color.formatHex();
+};
